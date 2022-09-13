dec984cfbb37b344604433971ade2c59fd9e7214
wilcoxrobotics <teamrobotics8872@gmail.com>
2022-09-13 15:47:27 -0700

    created class

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main {
+
+    private static int simTimes = 1000;
+
+    public static void main(String[] args) {
+        //TODO fill in the code needed to simulate the game
+
+        int maxScore = 0;
+    }
+
+    //strategy one
+    public int strat1() {
+        Random rand = new Random();
+        int avgScore = 0;
+
+        //TODO actions that earns scores
+        //TODO add them by probability
+
+
+        return avgScore;
+    }
+}
